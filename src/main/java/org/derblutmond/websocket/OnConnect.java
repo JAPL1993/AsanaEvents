df4a27cd298b9ac3bdeb41af8f91f14213267067
JAPL1993 <108290394+JAPL1993@users.noreply.github.com>
2023-05-19 18:30:11 -0600

    record

--- src/main/java/org/derblutmond/websocket/OnConnect.java
+++ src/main/java/org/derblutmond/websocket/OnConnect.java
@@ -5,16 +5,11 @@ import org.derblutmond.log.ConsoleLogger;
 
 import java.util.logging.Level;
 
-public class OnConnect implements Emitter.Listener {
-    final public String namespace;
-
-    public OnConnect(String namespace) {
-        this.namespace = namespace;
-    }
+public record OnConnect(String namespace) implements Emitter.Listener {
 
     @Override
     public void call(Object... args) {
         ConsoleLogger console = new ConsoleLogger(OnConnect.class);
-        console.log(Level.INFO, "Connected to "+namespace);
+        console.log(Level.INFO, "Connected to " + namespace);
     }
 }
